9b34592d0281b3e15a02ee53d7d8d5a263dc3a6b
mtugb laptop <main@mtugb.com>
2026-03-04 17:57:25 +0900

    feat:initialization

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import process from "node:process";
+const vite_pug_adapter = {
+  name: "vite-pug-adapter",
+  configureServer: function (server) {
+    server.middlewares.use(async (req, res, next) => {
+      // normal page
+      if (/^\/[a-zA-Z]*$/.test(req.url)) {
+        const pug_path = req.url === "/" ? "/index" : req.url;
+        try {
+          let html = pug.renderFile(`${process.cwd()}${pug_path}.pug`);
+          res.setHeader("Content-Type", "text/html");
+          // enable hot reloading
+          html = await server.transformIndexHtml(req.url, html);
+          res.end(html);
+        } catch (e) {
+          // throw error to vite ( this func mutate error into explict way)
+          server.ssrFixStacktrace(e);
+          next(e);
+        }
+      } else {
+        next();
+      }
+    });
+
+    server.watcher.add(`${process.cwd()}/**/*.pug`);
+
+    server.watcher.on("change", (file) => {
+      if (file.endsWith(".pug")) {
+        server.ws.send({
+          type: "full-reload",
+          path: "*",
+        });
+      }
+    });
+  },
+};
+export default vite_pug_adapter;
